c9f05714f2b92d682d5c369f408eccd9668e52dd
Ayush-Mgr <yush_mgr@gmail.com>
2024-06-12 01:26:26 +0530

    llH

--- Data Algorithms in c/link_list_copy.c
+++ Data Algorithms in c/link_list_copy.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct node
 {
     int data;
     struct node* next;
 }*head,*second,*temp;
+
+void InsertHead(struct node* second){
+    second->next = head;
+    head = second ;  
+
+}
+
 
 int main(){ 
     char opt;
@@ -24,13 +30,25 @@ int main(){
             temp = second ;
         }
         else{
+            printf("\t at Head (H) Or continue (c):\n ");
+            scanf(" %c",&opt);
+            if (opt == 'H')
+            {
+                InsertHead(second);
+            }
+            else if(opt == 'C'){
+            
 
             temp->next = second; // temp has the existing value of previous Second and it gives the previous second.next the pointer value of next second
                                  
             temp = second; //now temp is set to recently created second with second.next null 
+         }
         }
-        printf("contine Y/N:\n ");
+        printf("\tcontine Y:\n ");
+        printf("\texit N:\n ");
         scanf(" %c",&opt);
+        
+        
     } while (opt != 'N');
     temp= head;
     int i =0 ;
@@ -55,29 +73,29 @@ int main(){
 
 input the data:
  12
-contine Y/N:
+        contine Y:
+        exit N:
+ Y
+
+input the data:
+ 23
+         at Head (H) Or continue (c):
+ H
+        contine Y:
+        exit N:
  Y
 
 input the data:
  33
-contine Y/N:
- Y
-
-input the data:
- 32
-contine Y/N:
- Y
-
-input the data:
- 11
-contine Y/N:
+         at Head (H) Or continue (c):
+ C
+        contine Y:
+        exit N:
  N
 
-1 data is  12
-2 data is  33
-3 data is  32
-4 data is  11
-
+1 data is  23
+2 data is  12
+3 data is  33
 */
 
 
